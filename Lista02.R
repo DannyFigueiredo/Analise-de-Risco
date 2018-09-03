b3f36865588e457e89230904e38b12c916ac21ed
Danielle Figueiredo <dannyfig.df@gmail.com>
2018-09-03 20:42:22 -0300

    Questão 2 - Aplicando TCL

--- Lista02.R
+++ Lista02.R
@@ -19,3 +19,30 @@ q1 <- function(){
   
   View(frequenciaDeCaras)
 }
+
+################################# QUESTÃO 2 #################################
+
+tcl <- function(tamanhoAmostra){
+  # Esta funcao toma uma amostra de uma variável aleatoria que representa
+  # a soma de 12 outras variáveis aleatórias uniformemente distribuidas entre 0 e 1
+  
+  x <- replicate(tamanhoAmostra, sum(runif(12)))
+  media_x <- mean(x)
+  var_x <- var(x)
+  
+  hist(x, xlab = "Possíveis Valores de X", ylab = "Frequência dos Valores de X",
+       main = "Distribuição de X", col = "grey",
+       sub = paste("Média de X: ", media_x, " e Variancia de X: ", var_x))
+}
+
+monteCarlo <- function(){
+  
+}
+
+q2 <- function(){
+  # Esta função toma por default o tamanho da amostra como 50
+  amostra = 50
+  tcl(amostra)
+  
+  
+}
